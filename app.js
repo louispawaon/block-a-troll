b53e6854119994b57f482e5fa0650199a08f20f5
louispawaon <lpawaon@gmail.com>
2022-01-23 21:24:34 +0800

    update packages

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const http = require('http');
 const express = require('express'); 
 const cors = require('cors');
-const app = express();              
+const path = require('path');
+const bodyParser = require('body-parser')
+const app = express(express.json());              
 const port = 3000;                  
+const twit = require('twit');
+require('dotenv').config();
 
-app.get('/', function (req, res) {
-    res.send('Hello')
-  })
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+var T = new twit({
+    consumer_key:process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret:process.env.TWITTER_CONSUMER_SECRET,
+    access_token:process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret:process.env.TWITTER_ACCESS_SECRET
+});
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname,'./public/index.html'));
+});
+
+app.post('/',function(req,res){
+    console.log(req.body.name);
+    res.send('added');
+});
 
 app.listen(port, () => {            
     console.log("Now listening on port: ${port}"); 
 });
+
