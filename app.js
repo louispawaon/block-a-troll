f28668d4a88cc3a649d6916a3c2b9f683ccadf28
louispawaon <lpawaon@gmail.com>
2022-02-04 21:01:05 +0800

    established basic functionalities - a lot lacking pa so chill

--- app.js
+++ app.js
@@ -1,70 +1,75 @@
 const express = require('express'); 
 const app = express(express.json());              
 const port = 3000;                  
-const twit = require('twit');
 const passport = require('passport');
-var Strategy = require('passport-twitter').Strategy;
 var session = require('express-session');
 
-
-
+const axios = require('axios');
 const http = require('http');
 const cors = require('cors');
 const path = require('path');
 const bodyParser = require('body-parser')
 
 require('dotenv').config();
-
-//var authenticate = require('./public/routes/auth.js');
-//var index = require('./public/routes/index.ejs');
+require('./passport')
 
 app.set('views', './views/routes');
-app.set('view-engine', 'ejs');
+app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-passport.use(new Strategy({
-    consumerKey: process.env.TWITTER_CONSUMER_KEY,
-    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-    callbackURL: 'http://localhost:3000/twitter/return'
-}, function(token, tokenSecret, profile, callback) {
-    return callback(null, profile);
-}));
-
-passport.serializeUser(function(user, callback) {
-    callback(null, user);
-})
-
-passport.deserializeUser(function(obj, callback) {
-    callback(null, obj);
-})
-
-var T = new twit({
-    consumer_key:process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret:process.env.TWITTER_CONSUMER_SECRET,
-    access_token:process.env.TWITTER_ACCESS_TOKEN,
-    access_token_secret:process.env.TWITTER_ACCESS_SECRET,
-    client_id:process.env.TWITTER_CLIENT_ID,
-    client_secret:process.env.TWITTER_CLIENT_SECRET
-});
-
+//Session Creator
 app.use(session({secret: 'whatever', resave: true, saveUninitialized: true}))
 app.use(passport.initialize())
 app.use(passport.session())
 
-
-app.get('/', (req, res) => res.render('index.ejs'));
-
-app.get('/twitter/login', passport.authenticate('twitter'))
-
+//Twitter Authentication
+app.get('/twitter/login', passport.authenticate('twitter'));
 app.get('/twitter/return', passport.authenticate('twitter', {
-    failureRedirect: '/'
+    failureRedirect: '/error'
 }), function(req, res) {
-    res.redirect('/')
-})
-
-app.listen(port, () => {            
-    console.log("Now listening on port: ${port}"); 
+    res.redirect('/main');
 });
 
+//GET Routes
+app.get('/', (req, res) => res.render('index'));
+
+app.get('/main', function(req, res){
+    if(!req.user){
+        res.redirect('/error');
+        return;
+    }
+    else{
+        res.render(('main'),{username: req.user.username});
+    }
+});
+
+app.get('/error', (req, res) => res.render('error'));
+app.get('/end', (req, res) => res.render('end'));
+
+//POST Routes
+app.post('/block', function(req, res){
+    const account=req.body.account;
+    getUser(account);
+    res.redirect('/end');
+});
+
+app.post('/blockMass', function(req, res){
+    const hashtag=req.body.hashtag;
+    getHashtag(hashtag);
+    res.redirect('/end');
+});
+
+//For blocking specific user
+
+
+app.listen(port, () => {            
+    console.log(`Now listening on port: ${port}`); 
+});
+
+//check if ang user na i attempt i block kay blocked na daan
+//optimize everything with async-await if applicable sa program
+//bootstrap ejs files kay wth ui
+//choices para sa block function
+//delete useless dependencies
---
+++ passport.js
@@ -0,0 +1,64 @@
+const passport = require('passport');
+const Strategy = require('passport-twitter').Strategy;
+const twit = require('twit');
+
+passport.serializeUser(function(user, callback) {
+    callback(null, user);
+})
+
+passport.deserializeUser(function(obj, callback) {
+    callback(null, obj);
+})
+
+passport.use(new Strategy({
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    callbackURL: 'http://localhost:3000/twitter/return'
+}, function(token, tokenSecret, profile, callback) {
+    console.log(token, tokenSecret);
+    
+    var client = new twit({
+        consumer_key:process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret:process.env.TWITTER_CONSUMER_SECRET,
+        access_token:token,
+        access_token_secret:tokenSecret,
+        client_id:process.env.TWITTER_CLIENT_ID,
+        client_secret:process.env.TWITTER_CLIENT_SECRET
+    });
+
+    
+    getUser=(account)=>{
+        console.log(account);
+        const params = {screen_name: account};
+        //put if statements
+        client.post('blocks/create', params, function(err, data, response) {
+            console.log(data)
+        });
+    }
+    
+    //For blocking all users under hashtag
+    getHashtag=(hashtag)=>{
+        const userList=[];
+        const params = {q: '#'+hashtag, count:50, result_type:'mixed'};
+        client.get('search/tweets', params, function(err, data, response) {
+                var userResult=JSON.stringify(data.statuses);
+                var users=JSON.parse(userResult);
+                users.forEach(x=>userList.push(x.user.screen_name));
+                console.log(userList);
+                blockMany(userList);
+        });  
+    }
+    
+    blockMany=(userList)=>{
+        userList.forEach(function(x){
+            const params={screen_name: x};
+            //block time
+            client.post('blocks/create', params, function(err, data, response) {
+                console.log(data)
+            });
+        });  
+    }
+
+    return callback(null, profile);
+}));
+
